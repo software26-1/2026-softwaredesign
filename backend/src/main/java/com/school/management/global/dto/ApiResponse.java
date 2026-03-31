bac9cf077058c3efed128438f2cf691cee971861
이정 <dlwjd8023@gmail.com>
2026-03-31 15:32:07 +0900

    feat: global 클래스 생성 - dto: ApiResponse, entity: BaseEntity, exceprion: Global ExceptionHandler

---
+++ backend/src/main/java/com/school/management/global/dto/ApiResponse.java
@@ -0,0 +1,29 @@
+package com.school.management.global.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    // 데이터 있는 성공 응답
+    // "T 타입을 받아서, T 타입이 담긴 ApiResponse를 반환해"
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        return new ApiResponse<>(true, message, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+
+}
---
+++ backend/src/main/java/com/school/management/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.school.management.global.exception;
+
+
+import com.school.management.global.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // ? 는 "어떤 타입이든 상관없어" 라는 뜻
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<?>> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("서버 내부 오류가 발생했습니다."));
+    }
+}
